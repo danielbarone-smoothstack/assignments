7ccd69be7277895c92f032a0bf4d311fb630295d
danielbarone <dev.danielbarone@gmail.com>
2020-10-14 07:22:14 -0400

    fin exercise 2

--- assignment_one/exercise_two/Driver.java
+++ assignment_one/exercise_two/Driver.java
@@ -4,64 +4,76 @@ public class Driver {
   public static void main(String[] args) {
     
     GameOfChips game;
-    int numPlayers = 0;
-
-    /* Prompt user for desired number of players */
     Scanner in = new Scanner(System.in);
-    System.out.print("Enter an even number of players: ");
-    String s = in.nextLine();
 
-    /* Initialize the game with given number of players */
-    try {
-      numPlayers = Integer.parseInt(s);
-      /* init with entered num of players */
-      if (numPlayers % 2 == 0) {
-        game = new GameOfChips(numPlayers);
-      /* Invalid input (odd number of players). Defaulting to 2. */
-      } else {
-        System.out.println("Invalid input. Number of players defaulting to 2.");
-        game = new GameOfChips(2);
-      }
-    /* Invalid input (not an integer), default to 2. */
-    } catch (Exception e) {
-      System.out.println("Invalid input. Number of players defaulting to 2.");
-      game = new GameOfChips(2);  // init game with 2 players
-    }
+    while (true) {
+      int numPlayers = 0;
 
-    /* Add players to the game */
-    for (int i = 1; i <= numPlayers; i++) {
-      do {
-        System.out.print("Enter the name of player " + i + ": ");
-        String name = in.nextLine();
-        if (game.addPlayer(name)) {
-          break;
-        }
-        String invalidPlayer = String.format("Cannot have two players named %s. ", name);
-        System.out.print(invalidPlayer);
-      } while (true);
-    }
+      /* Prompt user for desired number of players */
+      System.out.print("\nEnter an even number of players: ");
+      String s = in.nextLine();
 
-    /* Set initial number of chips */
-    do {
-      System.out.print("Enter the initial number of chips the pile contains: ");
-      s = in.nextLine();
+      /* Initialize the game with given number of players */
       try {
-        int numChips = Integer.parseInt(s);
-        if (numChips%2 != 0 && numChips > numPlayers) {
-          game.setNumChips(numChips);
-          break;
+        numPlayers = Integer.parseInt(s);
+        /* Invalid input (not even), default to 2. */
+        if (numPlayers % 2 != 0) {
+          System.out.println("Invalid input. Number of players defaulting to 2.");
+          numPlayers = 2;
         }
-        String output = String.format("You must start with an odd number of chips greater than or equal to %d. ", numPlayers + 1);
-        System.out.print(output);
+      /* Invalid input (not an integer), default to 2. */
       } catch (Exception e) {
-        System.out.print("Invalid input (not an integer). ");
+        System.out.println("Invalid input. Number of players defaulting to 2.");
+        numPlayers = 2;      
       }
-    } while (true);
-    
-    while (game.chipsRemaining > 0) {
-      game.printTurnInfo();
-      s = in.nextLine(); 
-    }
+      game = new GameOfChips(numPlayers);
 
+      /* Add players to the game */
+      for (int i = 1; i <= numPlayers; i++) {
+        do {
+          System.out.print("Enter the name of player " + i + ": ");
+          String name = in.nextLine();
+          if (name.length() == 0) {
+            name = String.format("Player %d", i);
+          }
+          if (game.addPlayer(name)) {
+            break;
+          }
+          String invalidPlayer = String.format("Cannot have two players named %s. ", name);
+          System.out.print(invalidPlayer);
+        } while (true);
+      }
+
+      /* Set initial number of chips */
+      do {
+        System.out.print("Enter the initial number of chips the pile contains: ");
+        s = in.nextLine();
+        try {
+          int numChips = Integer.parseInt(s);
+          if (numChips%2 != 0 && numChips > numPlayers) {
+            game.setNumChips(numChips);
+            break;
+          }
+          String output = String.format("You must start with an odd number of chips greater than or equal to %d. ", numPlayers + 1);
+          System.out.print(output);
+        } catch (Exception e) {
+          System.out.print("Invalid input (not an integer). ");
+        }
+      } while (true);
+      
+      while (game.chipsRemaining > 0) {
+        game.play();
+      }
+      
+      /* Play again? */
+      System.out.print("\nWould you like to play again? (y/n): ");
+      String playAgain = in.nextLine();
+      if (playAgain.toLowerCase().equals("y")) {
+        System.out.print("Restarting.");
+      } else {
+        System.out.print("Goodbye.");
+        break;
+      }
+    }
   }
 }
--- assignment_one/exercise_two/GameOfChips.java
+++ assignment_one/exercise_two/GameOfChips.java
@@ -13,6 +13,10 @@ class Player {
     String output = String.format("%s has %d chips.", name, numChips);
     System.out.println(output);
   }
+
+  public void addChips(int numChips) {
+    this.numChips += numChips;
+  }
 }
 
 public class GameOfChips {
@@ -36,22 +40,67 @@ public class GameOfChips {
     this.chipsRemaining = chipsRemaining;
   }
 
-  public void printTurnInfo() {
-    for (Map.Entry<String, Player> player : players.entrySet()) {
-      Player currPlayer = player.getValue();
-      currPlayer.printPlayer();
-    }
-    String output = String.format("There are %d chips remaining.", chipsRemaining);
-    System.out.println(output);
+  public void play() {
+    Iterator<Map.Entry<String, Player>> iterator = players.entrySet().iterator();
+    Scanner in = new Scanner(System.in);
+    while (iterator.hasNext()) {
+      System.out.println("\n==========================\n");
+      Map.Entry<String, Player> currPlayer = iterator.next();
 
-    int maxNumChips;
-    if (chipsRemaining % 2 == 0) {
-      maxNumChips = chipsRemaining / 2;
-    } else {
-      maxNumChips = Math.max(((chipsRemaining - 1)/2), 1);
+      for (Map.Entry<String, Player> player : players.entrySet()) {
+        Player p = player.getValue();
+        p.printPlayer();
+      }
+
+      if (chipsRemaining <= 0) {
+        Player winner = new Player("");
+        int currHighestScore = 0;
+        for (Map.Entry<String, Player> player : players.entrySet()) {
+          Player p = player.getValue();
+          if (p.numChips > currHighestScore && p.numChips%2 == 0) {
+            winner = p;
+            currHighestScore = p.numChips;
+          }
+        }
+        String gameOver = String.format("%s wins!\n", winner.name);
+        System.out.println(gameOver);
+        break;
+      }
+
+      String output = String.format("There are %d chips remaining.", chipsRemaining);
+      System.out.println(output);
+  
+      int maxNumChips;
+      if (chipsRemaining % 2 == 0) {
+        maxNumChips = chipsRemaining / 2;
+      } else {
+        maxNumChips = Math.max(((chipsRemaining - 1)/2), 1);
+      }
+      output = String.format("You may take any number of chips from 1 to %d. ", maxNumChips);
+      System.out.print(output);
+      int chipsSelected = 0;
+      do {
+        
+        System.out.print("How many will you take, " + currPlayer.getValue().name + "? ");
+        String s = in.nextLine();
+        try {
+          chipsSelected = Integer.parseInt(s);
+          if (chipsSelected >= 1 && chipsSelected <= maxNumChips) {
+            break;
+          }
+          output = String.format("Invalid entry. You must enter an integer in range 1 - %d. ", maxNumChips);
+          System.out.println(output);
+        } catch (Exception e) {
+          output = String.format("Invalid entry. You must enter an integer in range 1 - %d. ", maxNumChips);
+          System.out.println(output);
+        }
+      } while (true);
+      
+      currPlayer.getValue().addChips(chipsSelected);
+      chipsRemaining -= chipsSelected;
+
+      
     }
-    output = String.format("You may take any number of chips from 1 to %d. ", maxNumChips);
-    System.out.print(output + "How many will you take, " + "..." + "? ");
   }
   
 }
